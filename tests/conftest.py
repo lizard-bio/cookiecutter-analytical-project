317b7e2beb475ac4566ecb435f00228078eec44e
erik <erik.vandeputte@lizard.bio>
2022-06-16 14:54:21 +0200

    remove LICENSE for now

--- tests/conftest.py
+++ tests/conftest.py
@@ -10,7 +10,6 @@ args = {
     "lizard_code": "Liz.0.0",
     "project_name": "DrivenData",
     "author_name": "Jeff Bezos",
-    "open_source_license": "BSD-3-Clause",
     "python_interpreter": "python",
 }
 
