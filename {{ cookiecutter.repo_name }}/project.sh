2c81894ace7f7bd54251506303459a98c2994ea0
picousse <pieter.coussement@gmail.com>
2025-05-15 21:37:12 +0200

    update

--- {{ cookiecutter.repo_name }}/project.sh
+++ {{ cookiecutter.repo_name }}/project.sh
@@ -7,10 +7,10 @@ read -p "Enter your choice [1-2]: " choice
 
 case $choice in
     1)
-        cruft create git@github.com:lizard-bio/cookiecutter-analytical-project.git --directory="block/r_general"  --branch="feature/switch_to_functional_setup"
+        cruft create git@github.com:lizard-bio/cookiecutter-analytical-project.git --directory="block/r_general"  --checkout="feature/switch_to_functional_setup"
         ;;
     2)
-        cruft create git@github.com:lizard-bio/cookiecutter-analytical-project.git --directory="block/python_general"  --branch="feature/switch_to_functional_setup"
+        cruft create git@github.com:lizard-bio/cookiecutter-analytical-project.git --directory="block/python_general"  --checkout="feature/switch_to_functional_setup"
         ;;
     *)
         echo "Invalid choice."
