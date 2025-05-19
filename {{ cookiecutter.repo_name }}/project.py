1095b15169b4827cb9b97d8586ce1d8f6d7c0052
picousse <pieter.coussement@gmail.com>
2025-05-19 11:33:22 +0200

    questionnaire correctly

--- {{ cookiecutter.repo_name }}/project.py
+++ {{ cookiecutter.repo_name }}/project.py
@@ -1,18 +1,24 @@
 import questionary
 from cookiecutter.main import cookiecutter
 
+
 base_choices = [
-    questionary.Choice('Add a general python submodule', value=0),
-    questionary.Choice('Add a general R submodule', value=1),
-#    questionary.Choice('Add a spatial submodule', value=2)
+    questionary.Choice("Add a general python submodule", value=0),
+    questionary.Choice("Add a general R submodule", value=1),
+    #    questionary.Choice('Add a spatial submodule', value=2)
 ]
 
-base_selection = questionary.select(
-    "What do you want to do?",
-    choices=base_choices
-).ask()
+base_selection = questionary.select("What do you want to do?", choices=base_choices).ask()
 
 if base_selection == 0:
-    cookiecutter()
+    cookiecutter(
+        "lizard-bio/cookiecutter-analytical_project",
+        checkout="feature_switch_to_functional_setup",
+        directory="block/python_general",
+    )
 elif base_selection == 1:
-    cookiecutter()
+    cookiecutter(
+        "lizard-bio/cookiecutter-analytical_project",
+        checkout="feature_switch_to_functional_setup",
+        directory="block/R_general",
+    )
